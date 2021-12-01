1a2902bf4d0d727923a8b8dccd5b1ae5d15ecce3
Cal Krug <cal.krug@protonmail.com>
2021-11-30 22:03:54 -0600

    Finished navbar (responive)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../assets/Tux.svg';
+import Logo from '../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/Navbar.css';
@@ -11,13 +11,15 @@ function Navbar() {
             <img className="logo" src={ Logo } />
         </div>
         <div className="rightside">
-            <Link to="/"> Home</Link>
-            <Link to="/shop"> Shop</Link>
-            <Link to="/about"> About</Link>
-            <Link to="/contact"> Contact</Link>
-            <button className="hamburger-menu">
-                <ReorderIcon className="hamburger-menu" />
-            </button>
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <div className="mobile-menu">
+                <button className="burger-menu">
+                    <ReorderIcon />
+                </button>
+            </div>
         </div>
     </div>
     )
