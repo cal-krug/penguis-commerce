86856bd298f3f2531db9c574c1b3a7d1ea14a634
Cal Krug <cal.krug@protonmail.com>
2021-12-02 23:03:02 -0600

    Responsive Menu & Home Page Design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, {useState } from 'react'
 import Logo from '../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
+
 import '../styles/Navbar.css';
 
 function Navbar() {
+    // useState hook to open the hiddenLinks of the hamburger mobile menu
+    const [openLinks, setOpenLinks] = useState(false);
+    // function to toggle the navbar upon clicking the "ReorderIcon" statement
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks);
+    }
+
     return (
     <div className="navbar">
-        <div className="leftside">
+        {/* Turnary operator on openLinks state, i.e. {state ? "true" do this : "false" do this} */}
+        <div className="leftside" id={openLinks ? "open" : "close"}>
             <img className="logo" src={ Logo } />
+            <div className="hiddenLinks">
+                <Link to="/">Home </Link>
+                <Link to="/shop">Shop </Link>
+                <Link to="/about">About </Link>
+                <Link to="/contact">Contact </Link>
+            </div>
         </div>
         <div className="rightside">
             <Link to="/">Home</Link>
@@ -16,7 +31,7 @@ function Navbar() {
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
             <div className="mobile-menu">
-                <button className="burger-menu">
+                <button onClick={toggleNavbar} className="burger-menu">
                     <ReorderIcon />
                 </button>
             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import BannerImage from '../assets/astronautGunship.jpg';
+import "../styles/Home.css";
+
 
 function Home() {
     return (
-    <div>Hello World</div>
+    <div className="home">
+        <div
+        className="headerContainer"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+        >
+            <h1> STAR REACT Labs </h1>
+            <p> FOR CHRIST, HONOR, GLORY </p>
+            <Link to="menu">
+                <button> BUY PRODUCT </button>
+            </Link>
+        </div>
+    </div>
     )
 }
 
